a12bdae579b71e4ace51844c954fc9e6210694f0
AnatoliyKerosirov <anatoliy.kerosirov@gmail.com>
2020-06-18 16:50:24 +0300

    test save user on srver mongoDB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 module.exports.login = function(req, res){
     res.status(200).json({
         login: 'from controller'
@@ -5,7 +7,11 @@ module.exports.login = function(req, res){
 }
 
 module.exports.register = function(req, res){
-    res.status(200).json({
-        register: 'from controller'
+    const user = new User({
+        id: req.body.id,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
     })
+    user.save().then(() => console.log('User created'))
 }
