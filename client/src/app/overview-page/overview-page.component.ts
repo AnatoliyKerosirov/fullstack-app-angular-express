9637141fbc31d4fb0dcd423399a118ae0487b886
AnatoliyKerosirov <anatoliy.kerosirov@gmail.com>
2020-07-03 16:50:21 +0300

    Edit overview-page component: tapTarget element

--- client/src/app/overview-page/overview-page.component.ts
+++ client/src/app/overview-page/overview-page.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 
 import {AnalyticsService} from "../shared/services/analytics.service";
-import {MaterialService} from "../shared/classes/material.service";
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
 import {Observable} from "rxjs";
 import {OverviewPage} from "../shared/interfaces";
 
@@ -10,8 +10,9 @@ import {OverviewPage} from "../shared/interfaces";
   templateUrl: './overview-page.component.html',
   styleUrls: ['./overview-page.component.scss']
 })
-export class OverviewPageComponent implements OnInit {
-
+export class OverviewPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('tapTargetDiv') tapTargetRef: ElementRef
+  tapTarget: MaterialInstance
   dataOverview$: Observable<OverviewPage>
   day: Date
 
@@ -21,6 +22,18 @@ export class OverviewPageComponent implements OnInit {
   ngOnInit(): void {
     this.day = new Date()
     this.dataOverview$ = this.analyticsService.fetchOverview()
+  }
+
+  ngOnDestroy(): void {
+    this.tapTarget.destroy()
+  }
+
+  ngAfterViewInit(): void {
+    this.tapTarget = MaterialService.initTapTarget(this.tapTargetRef)
+  }
+
+  openInfo(){
+    this.tapTarget.open()
   }
 
 }
